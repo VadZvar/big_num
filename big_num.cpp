46d20a30b862e3c3d954a700b2015aad8126bfa3
VadZvar <vadzvar@gmail.com>
2015-11-28 17:20:44 +0600

    adding file

--- big_num.cpp
+++ big_num.cpp
@@ -65,15 +65,15 @@ BigNumber::operator = (const BigNumber& b) {
 }
 
 BigNumber::Compare(const BigNumber& b) {
-    if ((bn - en) == (b.bn - b.en)) {
-        for (base *tmp1 = bn, *tmp2 = b.bn; tmp1 >= en; --tmp1, --tmp2) {
+    if ((en - bn) == (b.en - b.bn)) {
+        for (base *tmp1 = en, *tmp2 = b.en; tmp1 >= bn; --tmp1, --tmp2) {
             if (*tmp1 > *tmp2) return 1;
             if (*tmp1 < *tmp2) return -1;
     }
         return 0;
 }
     else {
-        if ((bn - en) > (b.bn - b.en)) return 1;
+        if ((en - bn) > (b.en - b.bn)) return 1;
         else return -1;
     }
 }
@@ -100,4 +100,68 @@ bool BigNumber::operator == (const BigNumber& b) {
 
 bool BigNumber::operator != (const BigNumber& b) {
     return (Compare(b) != 0);
+}
+
+BigNumber BigNumber::operator + (const BigNumber& b) {
+    size_t size_max;
+    size_t size_min;
+    int i;
+    base *min, *max;
+    if (en - bn >= b.en - b.bn) {
+        size_max = en - bn + 1;
+        size_min = b.en - b.en + 1;
+        max = bn;
+        min = b.bn;
+    }
+    else {
+        size_max = b.en - b.bn + 1;
+        size_min = en - bn + 1;
+        max = b.bn;
+        min = bn;
+    }
+    BigNumber res(size_max + 1, 0);
+    dbase buffer = 0;
+    for (i = 0; i < size_min; ++i) {
+        buffer += (dbase)min[i] + (dbase)max[i];
+        res.bn[i] = (base)buffer;
+        buffer >>= BBITS;
+    }
+    for (; i < size_max; ++i) {
+        buffer += (dbase)max[i];
+        res.bn[i] = (base)buffer;
+        buffer >>= BBITS;
+    }
+    if (buffer) {
+        res.bn[size_max] = (base)buffer;
+        res.en = res.bn + size_max;
+    } else res.en = res.bn + size_max - 1;
+    
+    return res;
+}
+
+BigNumber BigNumber::operator - (const BigNumber& b) {
+    if (*this < b) throw "< 0";
+    size_t size_a = en - bn + 1;
+    size_t size_b = b.en - b.bn + 1;
+    int i;
+    dbase buffer = 0;
+    BigNumber res(size_a, 0);
+    for (i = 0; i < size_b; ++i) {
+        buffer = (dbase)bn[i] - (dbase)b.bn[i] - buffer;
+        res.bn[i] = (base)buffer;
+        buffer >>= BBITS;
+        buffer = (buffer)?1:0;
+    }
+    for (; i < size_a && buffer; ++i) {
+        buffer = (dbase)bn[i] - buffer;
+        res.bn[i] = (base)buffer;
+        buffer >>=BBITS;
+        buffer = (buffer)?1:0;
+    }
+    for (; i < size_a; ++i) res.bn[i] = bn[i];
+    if (i == size_a && !res.bn[size_a - 1] && size_a != 1) {
+        res.en = res.bn + size_a - 2;
+        for (; *res.en == 0 && res.en > res.bn; --res.en);
+    } else res.en = res.bn + size_a - 1;
+    return res;
 }
--- big_num.h
+++ big_num.h
@@ -4,13 +4,15 @@
 #include <iostream>
 #include <cstdint>
 
+#define BBITS 32
+
 typedef uint32_t base;
 typedef uint64_t dbase;
 
 class BigNumber {
     base *bn, *en, *ba, *ea; //bn, en - begin & end number, ba, ea - begin & end area
     public:
-    BigNumber(int, base);//выделяю память длины len и заполняю каждую базу длины fill, которую передаю в fill
+    BigNumber(int, base);//выделяю память длины len и заполняю каждую ячейку большого числа значением fill
     BigNumber(base);
     BigNumber(const BigNumber&);
     operator = (const BigNumber&);
@@ -20,6 +22,8 @@ class BigNumber {
     operator <= (const BigNumber&);
     operator == (const BigNumber&);
     operator != (const BigNumber&);
+    BigNumber operator + (const BigNumber&);
+    BigNumber operator - (const BigNumber&);
     ~BigNumber();
     private:
     BigNumber();
